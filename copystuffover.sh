5915f22b6c7dcc374100af3e8f229e8facbaade9
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-01-20 22:56:59 +0000

    script to copy correct stuff

---
+++ copystuffover.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp ../apertium-khk/apertium-khk.khk.lexc ./apertium-khk-kaz.khk.lexc
+cp ../apertium-khk/apertium-khk.khk.twol ./apertium-khk-kaz.khk.twol
+cp ../apertium-kaz/apertium-kaz.kaz.lexc ./apertium-khk-kaz.kaz.lexc
+cp ../apertium-kaz/apertium-kaz.kaz.twol ./apertium-khk-kaz.kaz.twol
