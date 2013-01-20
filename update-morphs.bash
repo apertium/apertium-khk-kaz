89025d656fdd6e65f65d68058fbe2ab9c2b4cbb0
Francis M. Tyers <ftyers@prompsit.com>
2013-01-20 21:06:14 +0000

    update files from update morphs

--- update-morphs.bash
+++ update-morphs.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#assuming that you have the whole apertium tree in your source dir. and you are in kaz-tat directory
+#assuming that you have the whole apertium tree in your source dir. and you are in khk-kaz directory
 
-python3 ../../trunk/apertium-tools/trim-lexc.py apertium-kaz-tat.kaz-tat.dix ../apertium-kaz/apertium-kaz.kaz.lexc ../apertium-tat/apertium-tat.tat.lexc
+python3 ../../trunk/apertium-tools/trim-lexc.py apertium-khk-kaz.khk-kaz.dix ../apertium-kaz/apertium-kaz.kaz.lexc ../apertium-khk/apertium-khk.khk.lexc
 
-cp /tmp/apertium-kaz.kaz.lexc.trimmed apertium-kaz-tat.kaz.lexc
-cp /tmp/apertium-tat.tat.lexc.trimmed apertium-kaz-tat.tat.lexc
+cp /tmp/apertium-kaz.kaz.lexc.trimmed apertium-khk-kaz.kaz.lexc
+cp /tmp/apertium-khk.khk.lexc.trimmed apertium-khk-kaz.khk.lexc
 
-cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-kaz-tat.kaz.twol
-cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-kaz-tat.kaz-tat.rlx
+cp ../apertium-kaz/apertium-kaz.kaz.twol apertium-khk-kaz.kaz.twol
+cp ../apertium-kaz/apertium-kaz.kaz.rlx apertium-khk-kaz.khk-kaz.rlx
 
-cp ../apertium-tat/apertium-tat.tat.twol apertium-kaz-tat.tat.twol
-cp ../apertium-tat/apertium-tat.tat.rlx apertium-kaz-tat.tat-kaz.rlx
+cp ../apertium-khk/apertium-khk.khk.twol apertium-khk-kaz.khk.twol
+cp ../apertium-khk/apertium-khk.khk.rlx apertium-khk-kaz.khk-kaz.rlx
 
 exit 0
 
