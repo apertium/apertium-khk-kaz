fda9dc7ce763177963d8eeda10facd77097ba559
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-11-03 19:02:52 +0000

    okay, don't need this

--- copystuffover.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp ../apertium-khk/khk.automorf.hfst ./khk-kaz.automorf.hfst
-cp ../apertium-khk/khk.autogen.hfst ./kaz-khk.autogen.hfst
-cp ../../languages/apertium-kaz/kaz.automorf.hfst ./kaz-khk.automorf.hfst
-cp ../../languages/apertium-kaz/kaz.autogen.hfst ./khk-kaz.autogen.hfst
